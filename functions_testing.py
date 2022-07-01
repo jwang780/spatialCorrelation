ddfec08b9255124450b0cb25d852fbe1e81bf7ad
Jianming <jwang780@gmail.com>
2022-06-30 21:07:02 -0400

    improved unit test for distance function to test different average distances

--- functions_testing.py
+++ functions_testing.py
@@ -8,12 +8,15 @@ def nearPSD_testing():
 
 def distance_testing():
     numberOfCases = 10
+    maxDiff = 0.1
+    centerLat = random.random() * (math.pi - maxDiff) - (math.pi - maxDiff) / 2
+    centerLon = random.random() * (math.pi - maxDiff) * 2 + maxDiff
     for i in range(numberOfCases):
-        lat1 = random.random() * math.pi - math.pi / 2
-        lon1 = random.random() * math.pi * 2
+        lat1 = random.random() * maxDiff * 2 - maxDiff + centerLat
+        lon1 = random.random() * maxDiff * 2 - maxDiff + centerLon
         origin = lat1, lon1
-        lat2 = random.random() * math.pi - math.pi / 2
-        lon2 = random.random() * math.pi * 2
+        lat2 = random.random() * maxDiff * 2 - maxDiff + centerLat
+        lon2 = random.random() * maxDiff * 2 - maxDiff + centerLon
         destination = lat2, lon2
         print("distance:" + str(distance(origin, destination)))
         print("distance dot:" + str(distance_dot(origin, destination)))
